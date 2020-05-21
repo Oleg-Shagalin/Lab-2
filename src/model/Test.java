ac7e6406330cbc944136b65ad9416625e2d97121
Oleg-Shagalin <62735543+Oleg-Shagalin@users.noreply.github.com>
2020-05-22 02:19:33 +0400

    Lab 2

---
+++ src/model/Test.java
@@ -0,0 +1,106 @@
+package model;
+
+import oop.model.*;
+
+public class Test {
+
+    public static void lab2tests() {
+        Tariff tariff = new IndividualsTariff();
+        Tariff tariff1 = new IndividualsTariff(3);
+        Tariff tariff2 = new IndividualsTariff(new Service[3]);
+
+        tariff.add(1, new Service());
+        for (Service s : tariff.getServices()) {
+            System.out.println(s.getName());
+        }
+
+        for (int i = 0; i < 8; i ++) {
+            tariff.add(new Service(String.valueOf(i), i));
+        }
+        for (Service s : tariff.getServices()) {
+            System.out.println(s.getName());
+        }
+
+        Service service = tariff.get(1);
+        System.out.println(service.getName());
+
+        service = tariff.get("3");
+        System.out.println(service.getName());
+
+        System.out.println("hasService " + "4: " + tariff.hasService("4"));
+
+        service = tariff.set(0, new Service());
+        System.out.println(service.getName());
+
+        service = tariff.remove(3);
+        System.out.println(service.getName());
+
+        service = tariff.remove("5");
+        System.out.println(service.getName());
+
+        System.out.println("size: " + tariff.size());
+
+        for (Service s : tariff.sortedServicesByCost()) {
+            System.out.println(s.getCost());
+        }
+
+        System.out.println(tariff.cost());
+
+
+        Tariff entityTariff = new EntityTariff();
+        Tariff entityTariff1 = new EntityTariff(tariff.getServices());
+        System.out.println(entityTariff1.remove(0).getName());
+        System.out.println(entityTariff1.remove("1").getName());
+        for (Service s : entityTariff1.getServices()) {
+            System.out.println(s.getName());
+        }
+        for (Service s : entityTariff1.sortedServicesByCost()) {
+            System.out.println(s.getName());
+        }
+
+
+        Account account = new IndividualAccount(3, new Person("Ivan", "Ivanov"));
+        Account account1 = new IndividualAccount(4, new Person("Petr", "Petrov"),
+                new IndividualsTariff());
+
+        System.out.println(account.getNumber());
+        System.out.println(account.getTariff().cost());
+
+
+        Account entityAccount = new EntityAccount(4,"Petr");
+        Account entityAccount1 = new EntityAccount(4,"Petr", entityTariff);
+        System.out.println(entityAccount.getNumber());
+        entityAccount.setTariff(entityTariff1);
+        System.out.println(entityAccount.getTariff().cost());
+
+        AccountsManager manager = new AccountsManager(3);
+        Account[] accounts = new Account[2];
+        accounts[0] = account;
+        accounts[1] = entityAccount;
+        AccountsManager manager1 = new AccountsManager(accounts);
+        System.out.println(manager.add(account));
+        System.out.println(manager.add(2, account1));
+        account = manager.get(0);
+        account = manager.set(2, account);
+        account = manager.remove(1);
+        System.out.println(manager.size());
+
+        accounts = manager.getAccounts();
+        for (Account a : accounts) {
+            if (a != null)
+                System.out.println(a.getNumber());
+        }
+
+        tariff = manager.getTariff(3);
+        System.out.println(tariff.cost());
+
+        tariff = manager.setTariff(3, tariff1);
+        System.out.println(tariff.cost());
+
+    }
+
+    @org.junit.Test
+    public void startTests() {
+        lab2tests();
+    }
+}
